f07f9599a2d373c2da305ec76f2d6b9c73e28d0b
Karolina <ksenkow@luc.edu>
2021-06-21 12:01:37 -0500

    argparse method

--- blast.py
+++ blast.py
@@ -1,6 +1,31 @@
 import os
+import argparse
 
 # set path
 path = os.getcwd()
 
-os.system('blastn -query ' + path + '\\query.txt' +  ' -subject ' + path + '\\subject.txt')
+# declare args
+args = argparse.Namespace
+
+parser = argparse.ArgumentParser(description='Choose database.') 
+parser.add_argument('--download','-d', type=int, required=False, dest="download",help='0 for 16S, 1 for bacterial genome data')
+
+args = parser.parse_args()
+
+def test0():
+    print('test0 data')
+
+def test1():
+    print('test1 data')
+
+if __name__ == '__main__':
+    choice = args.download
+    if choice == 0:
+        test0()
+
+    elif choice == 1:
+        test1()
+    else:
+        print("Please choose 0 for download and 1 for test data")
+
+#os.system('blastn -query ' + path + '\\query.txt' +  ' -subject ' + path + '\\subject.txt')
--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, url_for, render_template, request
 import os, subprocess
 from subprocess import Popen, PIPE
 from subprocess import check_output
-import linux
+import blast
 
 # set current path
 path = os.getcwd()
@@ -10,16 +10,28 @@ path = os.getcwd()
 
 app = Flask(__name__)
 
+#@app.route("/", methods=["POST","GET"])
+#def home():
+#    if request.method == "POST":
+#        with open('query.txt', 'w') as query: # query output file
+#            input1 = request.form["post1"]
+#            query.write(input1)
+#        with open('subject.txt', 'w') as subject: # subject output file
+#            input2 = request.form["post2"]
+#            subject.write(input2)
+#        return redirect(url_for("command_server", command=command_server))
+#    else:
+#        return render_template("index.html")
+
 @app.route("/", methods=["POST","GET"])
 def home():
     if request.method == "POST":
-        with open('query.txt', 'w') as query: # query output file
-            input1 = request.form["post1"]
-            query.write(input1)
-        with open('subject.txt', 'w') as subject: # subject output file
-            input2 = request.form["post2"]
-            subject.write(input2)
-        return redirect(url_for("command_server", command=command_server))
+        if request.form["radiobutton"]:
+            option = request.form['radiobutton']
+            if option == 'option1':
+                return redirect(url_for("command_server0", command=command_server0))
+            else:
+                return redirect(url_for("command_server1", command=command_server1))
     else:
         return render_template("index.html")
 
@@ -27,9 +39,14 @@ def home():
 def run_command(command):
     return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
 
-@app.route('/<command>')
-def command_server(command):
-    return run_command('python ' + path + '/blast.py')
+
+@app.route('/command0/<command>')
+def command_server0(command):
+    return run_command('python ' + path + '/blast.py -d 0')
+
+@app.route('/command1/<command>')
+def command_server1(command):
+    return run_command('python ' + path + '/blast.py -d 1')
 
 
 
