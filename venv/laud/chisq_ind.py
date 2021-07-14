4ee8072d4f5d6c4b1e634ac87c9d0bf10dde8e57
Ubuntu <ubuntu@ip-172-31-35-206.us-east-2.compute.internal>
2021-07-14 05:29:01 +0000

    debugged

--- venv/laud/chisq_ind.py
+++ venv/laud/chisq_ind.py
@@ -3,11 +3,11 @@ import numpy as np
 from scipy.stats import chi2_contingency
 import sys
 
-def chisq_ind(var):
+def chisq_ind():
     data = pd.read_csv("laud/df.csv")
     data["present"] = np.array(["yes" for i in range(len(data))])
     data.loc[data["taxa_count"] == 0, ["present"]] = "no"
-    contingency = pd.crosstab(data[var], data["present"])
+    contingency = pd.crosstab(data["cure_status"], data["present"])
     c, p, dof, expected = chi2_contingency(contingency)
     string = "Test statistic: " + str(c) + "\n" + "P-value: " + str(p) + "\n" + "Degrees Freedom: " + str(dof)
     f = open("laud/result.txt", "w+")
@@ -15,4 +15,4 @@ def chisq_ind(var):
     f.write("\n"+ string)
     f.close()
 
-chisq_ind(str(sys.argv[1]))
+chisq_ind()
