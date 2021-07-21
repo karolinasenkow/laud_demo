d77f529efb3ceb59b658973564470d04c0264e6c
Ubuntu <ubuntu@ip-172-31-35-206.us-east-2.compute.internal>
2021-07-21 13:19:48 +0000

    commented out new routes

--- venv/laud/routes.py
+++ venv/laud/routes.py
@@ -347,24 +347,24 @@ def command_server11(command):
         content = file.read()
     return render_template("blast_results.html", content = content)
 
-#@app.route("/command12/<command>")
-#def commmand_server12(command):
-#    run_command("python3 " + path + "/laud/ML.py -d 1")
-#    with open(path + "/laud/ML_knn_outfile.txt", "r") as file:
-#        content = file.read()
-#    return render_template("blast_results.html", content = content)
-
 @app.route("/command12/<command>")
-def command_server12(command):
-    run_command("python3 " + path + "/laud/dim_red_df.py")
-    return redirect(url_for("command_server13", command = command_server13))
-
-@app.route('/command13/<command>')
-def command_server13(command):
-    run_command('python3 ' + path + '/laud/knn.py')
-    with open(path + "/laud/ML_knn_outfile.txt","r") as file:
+def commmand_server12(command):
+    run_command("python3 " + path + "/laud/ML.py -d 1")
+    with open(path + "/laud/ML_knn_outfile.txt", "r") as file:
         content = file.read()
     return render_template("blast_results.html", content = content)
+
+#@app.route("/command12/<command>")
+#def command_server12(command):
+#    run_command("python3 " + path + "/laud/dim_red_df.py")
+#    return redirect(url_for("command_server13", command = command_server13))
+
+#@app.route('/command13/<command>')
+#def command_server13(command):
+#    run_command('python3 ' + path + '/laud/knn.py')
+#    with open(path + "/laud/ML_knn_outfile.txt","r") as file:
+#        content = file.read()
+#    return render_template("blast_results.html", content = content)
 
 @app.route("/clear")
 def clear():
