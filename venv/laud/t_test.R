a9945731a1ed9ee5183403cc263649c2edc2f97b
Ubuntu <ubuntu@ip-172-31-35-206.us-east-2.compute.internal>
2021-07-22 15:39:44 +0000

    added wilcoxon test

--- venv/laud/t_test.R
+++ venv/laud/t_test.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 path = getwd()
 full_path <- paste(path, "laud", "t_test_df.csv", sep="/")
 data <- read.csv(file = full_path, header=TRUE)
@@ -10,12 +12,35 @@ t_test_boxplot <- paste(path, 'laud', 'static', 'images', 'graphs', 'boxplot.png
 # 1. Open jpeg file
 png(t_test_boxplot)
 # 2. Create the plot
-boxplot(taxa_count~cure_status)
+#boxplot(taxa_count~cure_status)
+ggplot(data, aes(x = cure_status, y = taxa_count)) +
+	geom_boxplot() + 
+	theme_bw() 
 # 3. Close the file
 dev.off()
 
+uni <- unique(data[c("cure_status")])[,1]
+vector = vector()
+for (i in 1:length(uni)) {
+	cat <- subset(data, cure_status == uni[i])
+	num = nrow(cat)
+	vector[i] <- num
+}
+
+bool <- vector < 25
+
 sink(output)
 
-t.test(taxa_count~cure_status, mu=0, alt="two.sided", conf=0.95, var.eq=F, paired=F)
+if (length(which(bool)) == 0) {
+	t.test(taxa_count ~ cure_status, mu = 0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+} else {
+	wilcox.test(taxa_count ~ cure_status, mu = 0, alt = "two.sided", conf = 0.95, paired = F)
+}
 
 sink()
+
+#sink(output)
+
+#t.test(taxa_count~cure_status, mu=0, alt="two.sided", conf=0.95, var.eq=F, paired=F)
+
+#sink()
